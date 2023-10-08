1f008acc95ae88bb1123be07d5fa5aebed17a16e
Денис <114943705+Dens1k@users.noreply.github.com>
2023-10-08 15:38:49 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,13 @@
+#work with index for DF
+list_index = list('qwqityui')
+
+index_data = pd.Index(list_index,
+                      dtype = np.float32,
+                      name = 'rows'
+                     )
+
+columns_data = pd.Index(['col_1', 'col_2', 'col_3'],
+                        name = 'cols'
+                       )
+
 
