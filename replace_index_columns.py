724c2e1c9febfc32b5e4635b5bc3d451f4e7a5f8
Денис <114943705+Dens1k@users.noreply.github.com>
2023-10-08 19:29:41 +0300

    Update replace_index_columns.py

--- replace_index_columns.py
+++ replace_index_columns.py
@@ -8,6 +8,7 @@ df.set_index(['col_1'], #set col in index
 df.reset_index(['col_1']) #doing columns index
 #if method stay empty, all index will be columns 
 
+#operation with DF 
 df['col_1'] #for select column (if < 2 it will be Series)
 df[['col_1']] #for select column (this for DF)
 df[['col_1', 'col_2']] = 1  #for select column (this for DF), and can be give new value
@@ -15,3 +16,5 @@ df['NEW_COL'] = 2 #for creating a new col
 df['NEW_COL'] = df['col_1'] #can use existing col 
 del df['NEW_COL'] #for del col 
 df[[False, True, True, False, True, False]] = 100 #for operation with row where index is True 
+df[[False, True, True, False, True, False]][['col_1']] #we can take row AND column from this row 
+df.col_1 #select col
