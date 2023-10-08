ea32b05fde9671d1433e10e0ce7c60081f8eb2b5
Денис <114943705+Dens1k@users.noreply.github.com>
2023-10-08 22:49:08 +0300

    Update arifmetic_operator.py

--- arifmetic_operator.py
+++ arifmetic_operator.py
@@ -1,1 +1,14 @@
-
+series+5 #work for everyone row series 
+series + [1,2,3,4,5] #operation work each index value + value 
+series + series #+ between series where index = in this series 
+df + 10 #each value will be + 
+df + [1,2,3] #each list value + index column from df 
+df1 + df2 #each value +, if 1 DF doesnt have same column in resuli will be null in tris col/rows
+add() # +
+#df.add(100); df.add([10,20,30],axis=1), df.add(df_2), series.add(df['col'])
+#df.add([10,20,30,40,50],axis=0, fill_value=777 *change NaN on your value)
+sub() # -
+div() # / 
+mul() # * 
+pow() # ** 
+mod() # %
