7549aea9f376fd70d3936c01c880ddeb134344f2
Денис <114943705+Dens1k@users.noreply.github.com>
2023-10-09 23:40:42 +0300

    Create head,info,describe.py

---
+++ head,info,describe.py
@@ -0,0 +1,17 @@
+info() #give information about DF : 
+# 1. Class object 
+# 2. info about Index, count, start and finish index 
+# 3. count column 
+# 4. info about columns: name, count without NaN, type data
+# 5. type date and count 
+# 6. memory DF 
+
+head(), tail() #top or bottom rows 
+
+df.describe() #get statistic each column data from Int columns 
+# count 
+# mean - avg
+# std 
+# min
+# 20,50,75% 
+# max 
